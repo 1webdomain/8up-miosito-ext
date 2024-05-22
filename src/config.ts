1606479241dc27f02d446e7eb1ddf190c0ac4945
1webdomain <1webdomain@gmail.com>
2024-05-22 16:02:52 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,42 +2,24 @@ import type { Config } from './types'
 
 export const config: Config = {
   settings: {
-    title: 'Status Page Pro',
+    title: 'URL Monitor Pro',
     url: 'https://cf-worker-status-page-pro-production.yunsii.workers.dev',
     displayDays: 90,
     collectResponseTimes: true,
   },
   monitors: [
     {
-      id: 'workers.cloudflare.com',
-      url: 'https://workers.cloudflare.com',
-      description: 'You write code. They handle the rest.',
+      id: 'montalti.win',
+      url: 'https://montalti.win',
+      description: 'Monitoraggio in corso 90gg check ogni 8 min.',
       followRedirect: false,
     },
     {
-      id: 'www.cloudflare.com',
-      url: 'https://www.cloudflare.com',
-      description: 'Built for anything connected to the Internet.',
+      id: 'www.montalti.eu',
+      url: 'https://www.montalti.eu',
+      description: 'Monitoraggio in corso 90gg check ogni 8 min.',
     },
-    {
-      id: 'blog.cloudflare.com',
-      url: 'https://blog.cloudflare.com',
-      name: 'The Cloudflare Blog',
-    },
-    {
-      id: 'google',
-      url: 'https://www.google.com/',
-      name: 'Google',
-      followRedirect: true,
-    },
-    {
-      id: 'bilibili',
-      url: 'https://www.bilibili.com/',
-    },
-    {
-      id: 'GitHub',
-      url: 'https://github.com/',
-    },
+
   ],
   monitorsCsvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSnewwW9OuXgtuutyYSfFJ_AZdI-UpkUjP2wWi-zZWM3MKa8IzBceWCe9qB_-Lmk-S7mSFgqKVnokam/pub?gid=0&single=true&output=csv',
 }
